b1a53b65d927031790ed4180627f2a13eeed4c95
chamujai <40738507+chamujai@users.noreply.github.com>
2024-10-14 21:59:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-
-"""Main application file"""
+import os
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/<random_string>')
-def returnBackwardsString(random_string):
-    """Reverse and return the provided URI"""
-    return "".join(reversed(random_string))
+@app.route('/')
+def SayHello():
+    return f"{os.environ.get('MSG')}"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+if __name__ == '__main___':
+    app.run(debug=True, port=5000, host='0.0.0.0')
